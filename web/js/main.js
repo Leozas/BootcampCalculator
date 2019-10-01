889bb659d93bf3471193023234d3573c5eb6f258
Michael Loyselle <52831149+Leozas@users.noreply.github.com>
2019-10-01 15:39:00 -0400

    UI Update .001 - Foundationals

--- web/js/main.js
+++ web/js/main.js
@@ -1,1 +1,22 @@
 //structure = var, then defined func, then overall func
+
+/*
+divApp.appendChild( newrow(),
+                    newrow(
+                        newcol(
+                            newrow(outputfield)
+                            newrow(inputfield(form))
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            newrow(
+                                newcol(), newcol(),newcol()
+                            )
+                        newcol(
+                            newrow(
+                                newbtn(edgecases as modal)
+                            )
+                        )
+                    newrow()
+*/
